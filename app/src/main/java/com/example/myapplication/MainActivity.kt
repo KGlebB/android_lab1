920f5498de1722d2d7c2f238e9068a7a946ef1e5
Gleb nikiforov <nikiforov02@list.ru>
2023-10-14 20:07:29 +0300

    CALCULATOR

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,31 +18,77 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var plusButton: Button;
+    private lateinit var minusButton: Button;
+    private lateinit var multiplyButton: Button;
+    private lateinit var divisionButton: Button;
+    private lateinit var inputFirstNumber: EditText;
+    private lateinit var inputSecondNumber: EditText;
+    private lateinit var resultTextView: TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.main)
+        setupVars()
+        initButtons()
+    }
+
+    private fun setupVars() {
+        plusButton = findViewById<Button>(R.id.plusButton)
+        minusButton = findViewById<Button>(R.id.minusButton)
+        multiplyButton = findViewById<Button>(R.id.multiplyButton)
+        divisionButton = findViewById<Button>(R.id.divisionButton)
+
+        inputFirstNumber = findViewById<EditText>(R.id.inputFirstNumber)
+        inputSecondNumber = findViewById<EditText>(R.id.inputSecondNumber)
+
+        resultTextView = findViewById<TextView>(R.id.resultTextView)
+    }
+
+    private fun initButtons() {
+        plusButton.setOnClickListener {
+            handlePlus()
+        }
+        minusButton.setOnClickListener {
+            handleMinus()
+        }
+        multiplyButton.setOnClickListener {
+            handleMultiply()
+        }
+        divisionButton.setOnClickListener {
+            handleDivision()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name! Made by Gleb",
-            modifier = modifier
-    )
-}
+    @SuppressLint("SetTextI18n")
+    private fun setResultText(result: Double) {
+        resultTextView.text = "Результат: $result"
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+    private fun handlePlus() {
+        val first = inputFirstNumber.text.toString().toDouble()
+        val second = inputSecondNumber.text.toString().toDouble()
+        setResultText(first + second)
+    }
+
+    private fun handleMinus() {
+        val first = inputFirstNumber.text.toString().toDouble()
+        val second = inputSecondNumber.text.toString().toDouble()
+        setResultText(first - second)
+    }
+
+    private fun handleMultiply() {
+        val first = inputFirstNumber.text.toString().toDouble()
+        val second = inputSecondNumber.text.toString().toDouble()
+        setResultText(first * second)
+    }
+
+    private fun handleDivision() {
+        val first = inputFirstNumber.text.toString().toDouble()
+        val second = inputSecondNumber.text.toString().toDouble()
+        if (second != 0.0) {
+            setResultText(first / second)
+        } else {
+            setResultText(.0)
+        }
     }
 }
